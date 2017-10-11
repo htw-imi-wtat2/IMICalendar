073dfa80380fd41a6c50ae0e5b67e8c34d23d8cb
B Kleinen <kleinen@infrastructure.de>
2017-10-11 22:34:12 +0200

    root route, seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :planning_states
   resources :events
+  root 'events#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
