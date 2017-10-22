952b75c996135ce34cd257b8c8d5b694189c1f63
B Kleinen <kleinen@infrastructure.de>
2017-10-22 10:04:04 +0200

    A2-Devise: rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171011201354) do
+ActiveRecord::Schema.define(version: 20171022074621) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -31,6 +31,22 @@ ActiveRecord::Schema.define(version: 20171011201354) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_planning_states_on_event_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "first_name"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
