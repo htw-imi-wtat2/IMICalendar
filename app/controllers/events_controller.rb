2ff4f4f45634981f9aa1cde6f6c9c0e853a03c1e
B Kleinen <kleinen@infrastructure.de>
2017-10-26 12:06:23 +0200

    S005: created field for adding tags to event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -62,7 +62,7 @@ class EventsController < ApplicationController
   # PATCH/PUT /events/1
   # PATCH/PUT /events/1.json
   def update
-    puts @params.inspect
+    tags = params[:tags]
     respond_to do |format|
       if @event.update(event_params)
         format.html { redirect_to @event, notice: 'Event was successfully updated.' }
