eb9cb779e5c8abf16795fa5720a729eabeeb30b8
B Kleinen <kleinen@infrastructure.de>
2017-11-02 11:20:43 +0100

    S009: created route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :events
+  resources :categories
   get 'planning', to: 'events#planning'
   root 'events#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
