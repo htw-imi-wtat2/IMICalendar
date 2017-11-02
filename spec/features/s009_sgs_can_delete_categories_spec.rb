1ee656ab01505099fae64a4a35a43f1ba7a3ef8d
B Kleinen <kleinen@infrastructure.de>
2017-11-02 11:50:58 +0100

    S009 - test case needs to be logged in

--- spec/features/s009_sgs_can_delete_categories_spec.rb
+++ spec/features/s009_sgs_can_delete_categories_spec.rb
@@ -2,12 +2,20 @@
 
 require 'rails_helper'
 describe 'category maintenance' do
-  it 'shows categories on maintenance page' do
-    c_names = %w[A B C D].map { |s| "Category #{s}" }
-    c_names.map { |n| Category.create(name: n) }
-    visit categories_path
-    c_names.each do | category |
-      expect(page).to have_content(category)
+  context '(when logged in)' do
+    before :each do
+      user = User.create(email: 'someone@htw-berlin.de',
+                         password: 'geheimgeheim',
+                         password_confirmation: 'geheimgeheim')
+      login_as(user, scope: :user)
+    end
+    it 'shows categories on maintenance page' do
+      c_names = %w[A B C D].map { |s| "Category #{s}" }
+      c_names.map { |n| Category.create(name: n) }
+      visit categories_path
+      c_names.each do |category|
+        expect(page).to have_content(category)
+      end
     end
   end
 end
