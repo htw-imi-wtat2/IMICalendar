3aecd35386cae239f35846fd9abe1c36a696876e
B Kleinen <kleinen@infrastructure.de>
2017-11-02 11:49:29 +0100

    S009 - created Controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# Controller for Categories
+class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :destroy]
+  def index
+    @planning_states = PlanningState.all
+  end
+
+  def show; end
+
+  def destroy
+    @category.destroy
+    respond_to do |format|
+      format.html do
+        redirect_to categories_url,
+                    notice: 'Category was successfully destroyed.'
+      end
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+#  def category_params
+#    params.require(:planning_state).permit(:name)
+#  end
+end
