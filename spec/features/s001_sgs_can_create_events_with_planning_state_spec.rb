209ee4f2c55e4d564c7d3706d37c5fa4100478d1
B Kleinen <kleinen@infrastructure.de>
2017-11-23 07:13:53 +0100

    rubocop

--- spec/features/s001_sgs_can_create_events_with_planning_state_spec.rb
+++ spec/features/s001_sgs_can_create_events_with_planning_state_spec.rb
@@ -1,14 +1,17 @@
+# frozen_string_literal: true
 require 'rails_helper'
 describe 'S001: SGS or an Admin user' do
   context '(when logged in)' do
     before :each do
-      user = User.create(email: 'someone@htw-berlin.de', password: 'geheimgeheim', password_confirmation: 'geheimgeheim')
-      login_as(user, :scope => :user)
+      user = User.create(email: 'someone@htw-berlin.de',
+                         password: 'geheimgeheim',
+                         password_confirmation: 'geheimgeheim')
+      login_as(user, scope: :user)
     end
     it 'can create events with planning state' do
       visit new_event_path
       # save_and_open_page
-
+      
       # note that these are all ids rather than texts.
       fill_in 'event_title', with: 'IMI-Weihnachtsfeier'
       fill_in 'event_location', with: 'H001'
